f269148b4a88734fd149fea130d4d248edd75b89
marcnjaramillo <marcn.jaramillo@gmail.com>
2019-01-06 18:42:52 -0800

    Install dependencies and set up app

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div className="ui container">
+
+    </div>
+  );
+};
+
+export default App;
